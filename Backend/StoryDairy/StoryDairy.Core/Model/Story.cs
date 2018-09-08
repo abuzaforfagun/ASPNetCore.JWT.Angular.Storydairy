981dd9eac3fc21893b7936746299e47c7338a542
Fagun Rain <mdabuzaforfagun@gmail.com>
2018-09-08 23:33:31 +0600

    Add story model

---
+++ Backend/StoryDairy/StoryDairy.Core/Model/Story.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StoryDairy.Core.Model
+{
+    public class Story
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public DateTime DateTime { get; set; }
+    }
+}
