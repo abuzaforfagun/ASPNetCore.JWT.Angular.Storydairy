3dda6e8cb7e5dc3c13c8bb433878d89d6837fee6
Fagun Rain <mdabuzaforfagun@gmail.com>
2018-09-09 00:04:11 +0600

    Add story repository

---
+++ Backend/StoryDairy/StoryDairy.Core/Ripository/StoryRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using StoryDairy.Core.Model;
+using StoryDairy.Core.Presistance;
+
+namespace StoryDairy.Core.Ripository
+{
+    public class StoryRepository
+    {
+        private readonly StoryDbContext context;
+
+        public StoryRepository(StoryDbContext context)
+        {
+            this.context = context;
+        }
+
+        public void Add(Story story)
+        {
+            context.Add(story);
+        }
+    }
+}
