829c562962e8da47e2e27e6e5d929a01780c8cad
Fagun Rain <mdabuzaforfagun@gmail.com>
2018-09-09 00:04:16 +0600

    add unit of work

---
+++ Backend/StoryDairy/StoryDairy.Core/Ripository/UnitOfWork.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using StoryDairy.Core.Presistance;
+
+namespace StoryDairy.Core.Ripository
+{
+    public class UnitOfWork
+    {
+        private readonly StoryDbContext context;
+
+        public StoryRepository StoryRepository { get; set; }
+
+        public UnitOfWork(StoryDbContext context)
+        {
+            this.context = context;
+            StoryRepository = new StoryRepository(context);
+        }
+
+        public void Done()
+        {
+            context.SaveChanges();
+        }
+    }
+}
