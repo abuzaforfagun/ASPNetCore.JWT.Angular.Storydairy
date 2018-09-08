fde1c970f8765057a518f08a1d09907990cd85b9
Fagun Rain <mdabuzaforfagun@gmail.com>
2018-09-09 01:16:51 +0600

    map story and story resource

---
+++ Backend/StoryDairy/StoryDairy/MappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using StoryDairy.Core.Model;
+using StoryDairy.Core.Resources;
+
+namespace StoryDairy
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Story, StoryAddResource>().ReverseMap();
+        }
+    }
+}
