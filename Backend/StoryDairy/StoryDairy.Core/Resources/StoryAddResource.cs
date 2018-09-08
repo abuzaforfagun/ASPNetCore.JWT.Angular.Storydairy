beaa34caf1d4bf448e53f9097ee2140098081155
Fagun Rain <mdabuzaforfagun@gmail.com>
2018-09-09 01:16:41 +0600

    add story resource for add

---
+++ Backend/StoryDairy/StoryDairy.Core/Resources/StoryAddResource.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StoryDairy.Core.Resources
+{
+    public class StoryAddResource
+    {
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public DateTime DateTime { get; set; }
+    }
+}
