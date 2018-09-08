11240a140fae90de7814dfd257fdd84cf6ac66d4
Fagun Rain <mdabuzaforfagun@gmail.com>
2018-09-08 23:55:34 +0600

    configure db context

---
+++ Backend/StoryDairy/StoryDairy.Core/Presistance/StoryDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using StoryDairy.Core.Model;
+
+namespace StoryDairy.Core.Presistance
+{
+    public class StoryDbContext : DbContext
+    {
+        public DbSet<Story> Stories { get; set; }
+        public StoryDbContext(DbContextOptions<StoryDbContext> options)
+           : base(options)
+        {
+        }
+        
+    }
+}
