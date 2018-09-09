e6d6c981f61885349c4912ea1df102a5f1572c43
Fagun Rain <mdabuzaforfagun@gmail.com>
2018-09-10 03:04:48 +0600

    Add user table

--- Backend/StoryDairy/StoryDairy.Core/Migrations/20180909210403_AddTblUsers.Designer.cs
+++ Backend/StoryDairy/StoryDairy.Core/Migrations/20180909210403_AddTblUsers.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using StoryDairy.Core.Presistance;
 
 namespace StoryDairy.Core.Migrations
 {
     [DbContext(typeof(StoryDbContext))]
-    partial class StoryDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180909210403_AddTblUsers")]
+    partial class AddTblUsers
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Backend/StoryDairy/StoryDairy.Core/Presistance/StoryDbContext.cs
+++ Backend/StoryDairy/StoryDairy.Core/Presistance/StoryDbContext.cs
@@ -9,10 +9,17 @@ namespace StoryDairy.Core.Presistance
     public class StoryDbContext : DbContext
     {
         public DbSet<Story> Stories { get; set; }
+        public DbSet<User> Users { get; set; }
         public StoryDbContext(DbContextOptions<StoryDbContext> options)
            : base(options)
         {
         }
-        
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<User>()
+                .HasIndex(u => u.UserId)
+                .IsUnique();
+        }
     }
 }
