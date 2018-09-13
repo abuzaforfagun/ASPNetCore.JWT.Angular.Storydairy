a44b5e2e48a7dabc25cf649eed34e26f4f783c0f
Fagun Rain <mdabuzaforfagun@gmail.com>
2018-09-14 01:33:55 +0600

    disable tracking

--- FrontEnd/StoryDairy/src/app/app-routing.module.ts
+++ FrontEnd/StoryDairy/src/app/app-routing.module.ts
@@ -15,7 +15,6 @@ const appRoutes: Routes = [
     imports: [
         RouterModule.forRoot(
             appRoutes,
-            { enableTracing: true } // <-- debugging purposes only
         )
     ],
     exports: [
