1d0ca083d16461970181fc4035d2a0ccdf50025e
Fagun Rain <mdabuzaforfagun@gmail.com>
2018-09-14 01:33:03 +0600

    add routing

--- FrontEnd/StoryDairy/src/app/app-routing.module.ts
+++ FrontEnd/StoryDairy/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { PageNotFoundComponent } from 'src/app/page-not-found/page-not-found.component';
+
 
 const appRoutes: Routes = [
-    
+    // { path: 'crisis-center', component: CrisisListComponent },
+    { path: 'login', component: LoginComponent },
+    { path: '', redirectTo: '/login', pathMatch: 'full' },
+    { path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
