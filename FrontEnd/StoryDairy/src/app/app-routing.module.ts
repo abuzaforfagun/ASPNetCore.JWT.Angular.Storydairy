79043f912e2b04a51aea130b0b468ad35f4cfcaa
Fagun Rain <mdabuzaforfagun@gmail.com>
2018-09-14 01:30:14 +0600

    add app routing module

---
+++ FrontEnd/StoryDairy/src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+    
+];
+
+@NgModule({
+    imports: [
+        RouterModule.forRoot(
+            appRoutes,
+            { enableTracing: true } // <-- debugging purposes only
+        )
+    ],
+    exports: [
+        RouterModule
+    ]
+})
+export class AppRoutingModule { }
