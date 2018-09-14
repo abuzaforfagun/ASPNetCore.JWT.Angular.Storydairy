818ca9e7b2a8822aa4007e8ebed65da8aba477ba
Fagun Rain <mdabuzaforfagun@gmail.com>
2018-09-15 00:26:34 +0600

    use http service

--- FrontEnd/StoryDairy/src/app/services/story.service.ts
+++ FrontEnd/StoryDairy/src/app/services/story.service.ts
@@ -1,24 +1,16 @@
+import { HttpService } from './http.service';
 import { Story } from './../models/story';
 import { Injectable } from '@angular/core';
-import { Story } from '../models/story';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StoryService {
-  stories: Story[] = [];
-  constructor() { 
-    this.dummyStory();
-  }
-  dummyStory() {
-    const item = new Story();
-    item.id = 1;
-    item.title = 'Story 1';
-    item.description = 'Description of story 1';
-    item.datetime = new Date();
-    this.stories.push(item);
-    this.stories.push(item);
-    this.stories.push(item);
-    this.stories.push(item);
+  stories: any = [];
+  constructor(private httpService: HttpService) {
+    // this.dummyStory();
+    this.httpService.get('https://localhost:44399/api/stories').subscribe(data => {
+      this.stories = data;
+    });
   }
 }
